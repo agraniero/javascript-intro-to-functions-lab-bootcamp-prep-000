764e4f58a9c9aa0029b83949d8b1b4fcf311d81c
Anthony Graniero <ananfro@gmail.com>
2018-09-27 10:27:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var uppercase = "HELLO!";
+var lowercase = "hello!";
 function shout(string) {
   return string.toUpperCase();
 }
@@ -11,8 +13,6 @@ function logWhisper(string) {
   console.log(string.toLowerCase());
 }
 function sayHiToGrandma(string) {
-  
-  var lowercase = "hello!";
   if(string === lowercase){
   return("I can't hear you!");
   }
