7440c4efa6b24b5a85daf36a8b76d8f4058f37b5
Anthony Graniero <ananfro@gmail.com>
2018-09-27 10:13:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ function sayHiToGrandma(string) {
   var uppercase = "HELLO!";
   var lowercase = 'hello!';
   if(string === lowercase){
-  return('I can't hear you!');
+  return("I can't hear you!");
   }
   else if(string === uppercase){
-  return('YES INDEED!');
+  return("YES INDEED!");
   }
   else if(string === "I love you, Grandma."){
   return("I love you, too.");
